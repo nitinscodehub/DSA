0b87d61db461d4d20c3a2d3c5d15d32d47b4ae1b
nitinscodehub <dhurvenitin05@gmail.com>
2026-04-01 21:08:10 +0530

    Time: 1 ms (57.97%) | Memory: 21.9 MB (71.70%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        bool rows[9][9] = {false};
+        bool cols[9][9] = {false};
+        bool boxes[9][9] = {false};
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+
+                if (board[i][j] == '.') continue;
+
+                int num = board[i][j] - '1';  // 0 to 8
+                int boxIndex = (i / 3) * 3 + (j / 3);
+
+                // check duplicate
+                if (rows[i][num] || cols[j][num] || boxes[boxIndex][num]) {
+                    return false;
+                }
+
+                // mark visited
+                rows[i][num] = true;
+                cols[j][num] = true;
+                boxes[boxIndex][num] = true;
+            }
+        }
+
+        return true;
+    }
+};
