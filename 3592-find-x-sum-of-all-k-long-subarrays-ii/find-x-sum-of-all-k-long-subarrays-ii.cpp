e1f192b9390d1274c86acb55eeae6019fa32825c
nitinscodehub <dhurvenitin05@gmail.com>
2026-04-03 09:21:20 +0530

    Time: 889 ms (30.34%) | Memory: 350.1 MB (18.35%) - LeetSync

--- 3592-find-x-sum-of-all-k-long-subarrays-ii/find-x-sum-of-all-k-long-subarrays-ii.cpp
+++ 3592-find-x-sum-of-all-k-long-subarrays-ii/find-x-sum-of-all-k-long-subarrays-ii.cpp
@@ -1,107 +1,100 @@
 class Solution {
 public:
-    using pii = pair<int,int>; // {freq, value}
-    
-    struct cmp {
-        bool operator()(const pii &a, const pii &b) const {
-            if (a.first != b.first) return a.first > b.first;
-            return a.second > b.second;
-        }
-    };
-    
     vector<long long> findXSum(vector<int>& nums, int k, int x) {
-        int n = nums.size();
-        
         unordered_map<int,int> freq;
-        
-        multiset<pii, cmp> top, rest;
+
+        auto cmp = [](pair<int,int>& a, pair<int,int>& b){
+            if(a.first != b.first) return a.first < b.first; // freq small first
+            return a.second < b.second; // value small first
+        };
+
+        set<pair<int,int>> top, rest;
         long long sum = 0;
-        
-        auto add = [&](int val) {
+
+        auto add = [&](int val){
             int f = freq[val];
-            
-            pii old = {f, val};
-            pii nw = {f+1, val};
-            
-            if (f > 0) {
-                if (top.count(old)) {
-                    top.erase(top.find(old));
+            pair<int,int> old = {f, val};
+            pair<int,int> now = {f+1, val};
+
+            if(f > 0){
+                if(top.count(old)){
+                    top.erase(old);
                     sum -= 1LL * f * val;
                 } else {
-                    rest.erase(rest.find(old));
+                    rest.erase(old);
                 }
             }
-            
+
             freq[val]++;
-            
-            rest.insert(nw);
+
+            rest.insert(now);
         };
-        
-        auto remove = [&](int val) {
+
+        auto remove = [&](int val){
             int f = freq[val];
-            pii old = {f, val};
-            pii nw = {f-1, val};
-            
-            if (top.count(old)) {
-                top.erase(top.find(old));
+            pair<int,int> old = {f, val};
+            pair<int,int> now = {f-1, val};
+
+            if(top.count(old)){
+                top.erase(old);
                 sum -= 1LL * f * val;
             } else {
-                rest.erase(rest.find(old));
+                rest.erase(old);
             }
-            
+
             freq[val]--;
-            
-            if (freq[val] > 0) {
-                rest.insert(nw);
+            if(freq[val] > 0){
+                rest.insert(now);
             }
         };
-        
-        auto rebalance = [&]() {
-            while (top.size() < x && !rest.empty()) {
-                auto it = rest.begin();
-                pii p = *it;
+
+        auto rebalance = [&](){
+            while(top.size() < x && !rest.empty()){
+                auto it = prev(rest.end());
+                auto p = *it;
                 rest.erase(it);
                 top.insert(p);
                 sum += 1LL * p.first * p.second;
             }
-            
-            while (!top.empty() && !rest.empty()) {
-                auto worstTop = prev(top.end());
-                auto bestRest = rest.begin();
-                
-                if (*bestRest < *worstTop) break;
-                
-                pii t = *worstTop;
-                pii r = *bestRest;
-                
-                top.erase(worstTop);
-                rest.erase(bestRest);
-                
-                sum -= 1LL * t.first * t.second;
-                
-                top.insert(r);
-                rest.insert(t);
-                
-                sum += 1LL * r.first * r.second;
+
+            while(top.size() > x){
+                auto it = top.begin();
+                auto p = *it;
+                top.erase(it);
+                sum -= 1LL * p.first * p.second;
+                rest.insert(p);
+            }
+
+            if(!top.empty() && !rest.empty()){
+                auto t = *top.begin();
+                auto r = *prev(rest.end());
+
+                if(r.first > t.first || (r.first == t.first && r.second > t.second)){
+                    top.erase(top.begin());
+                    rest.erase(prev(rest.end()));
+
+                    sum -= 1LL * t.first * t.second;
+                    sum += 1LL * r.first * r.second;
+
+                    top.insert(r);
+                    rest.insert(t);
+                }
             }
         };
-        
+
         vector<long long> ans;
-        
-        for (int i = 0; i < n; i++) {
+
+        for(int i=0;i<nums.size();i++){
             add(nums[i]);
-            
-            if (i >= k) {
-                remove(nums[i - k]);
-            }
-            
+            if(i >= k) remove(nums[i-k]);
+
             rebalance();
-            
-            if (i >= k - 1) {
+
+            if(i >= k-1){
                 ans.push_back(sum);
             }
         }
-        
+
         return ans;
     }
 };
